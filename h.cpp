666bd199f2f3635bb13e4e57ad05dbe8654a6d30
mohammed arshan <pmohammedarshanraqeeb@gmail.com>
2026-07-18 15:12:27 +0530

    first cpp file committed into repository so yeah

---
+++ h.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    cout<<"this is first file an intuision and further will be topics such as recursion and hashing ";
+    cout<<"ehmmm im mohammed arshan raqeeb , so yeah ";
+}
