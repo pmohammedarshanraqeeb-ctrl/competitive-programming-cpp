91d6fbcd6ec455446249d3453482b5980581ebcb
mohammed arshan <pmohammedarshanraqeeb@gmail.com>
2026-07-25 00:40:05 +0530

    b

--- math/b.cpp
+++ math/b.cpp
@@ -4,3 +4,16 @@ loops
 
 #include<bits/stdc++.h>
 using namespace std;
+/*
+print the loop untill it reaches the goal number like 1.2.3.4.5 stop if its on 3
+*/int main(){
+for(;;){
+int x;
+cin>>x;
+    if(x==3){
+        break;
+    }
+    
+    cout<<x<<endl;
+}
+}
