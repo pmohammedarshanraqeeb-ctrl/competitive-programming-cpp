b521649dcc2e3b34d992e54894006bc17d4ff1ea
mohammed arshan <pmohammedarshanraqeeb@gmail.com>
2026-07-25 00:44:35 +0530

    d

---
+++ math/recursion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+/*this code is all about recursion and writing fibbonauchi numbers */
+int n ;
+cin>>n;
+
+for(int i =0;i<n;i++){
+    if(i<=1){
+        return 1;
+    }
+
+
+
+}
+
+
+}
